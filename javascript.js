5d08b1a52f4d962903028ac3cd73762f5603ddec
kiralysoka <159224863+kiralysoka@users.noreply.github.com>
2024-02-09 11:42:29 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-
-
 function myFunction() {
   var dark_theme_body = document.getElementById("container_main");
   dark_theme_body.classList.toggle("dark-mode-container-md");
@@ -9,15 +7,5 @@ function myFunction() {
 
   var dark_theme_nav = document.getElementById("navbar_01");
   dark_theme_nav.classList.toggle("dark-mode-nav")
-
-  var dark_theme_button_detail = document.getElementById("dark_label:after");
-  dark_theme_button_detail.classList.toggle("dark_theme_label");
-
-  var dark_theme_topics = document.getElementById("Navigator");
-  dark_theme_topics.classList.toggle("dark-nav-link");
+ 
 }
-  
-
-
-
-
